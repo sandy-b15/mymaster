5a29c214d2f84783534d65878789b7700de81bcd
sandeep <sandeep@appachhi.com>
2018-07-26 23:47:26 +0530

    test

---
+++ public/index.php
@@ -0,0 +1,72 @@
+<?php
+
+use Phalcon\Loader;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\Mvc\Application;
+use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
+// Define some absolute path constants to aid in locating resources
+define('BASE_PATH', dirname(__DIR__));
+define('APP_PATH', BASE_PATH . '/app');
+// ...
+
+$loader = new Loader();
+
+$loader->registerDirs(
+    [
+        APP_PATH . '/controllers/',
+        APP_PATH . '/models/',
+    ]
+);
+
+$loader->register();
+
+// ...
+
+// Create a DI
+$di = new FactoryDefault();
+// Setup the view component
+$di->set(
+    'view',
+    function () {
+        $view = new View();
+        $view->setViewsDir(APP_PATH . '/views/');
+        return $view;
+    }
+);
+
+$di->set(
+    'url',
+    function () {
+        $url = new UrlProvider();
+        $url->setBaseUri('/');
+        return $url;
+    }
+);
+
+$application = new Application($di);
+
+try {
+    // Handle the request
+    $response = $application->handle();
+    $response->send();
+} catch (\Exception $e) {
+    echo 'Exception: ', $e->getMessage();
+}
+
+
+// Setup the database service
+$di->set(
+    "db",
+    function () {
+        return new DbAdapter(
+            [
+                "host"     => "localhost",
+                "username" => "phpmyadmin",
+                "password" => "root",
+                "dbname"   => "testdb",
+            ]
+        );
+    }
+);
